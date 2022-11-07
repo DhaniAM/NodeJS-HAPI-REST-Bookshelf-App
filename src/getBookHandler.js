9545a9ad2e94ea5ad5a6a7854a6fb06d06b2420e
Dhani AM <dhani.a.mm@gmail.com>
2022-11-08 01:56:59 +0800

    added query paramenters but not working yet

--- src/getBookHandler.js
+++ src/getBookHandler.js
@@ -1,11 +1,119 @@
 const {bookshelf} = require("./bookshelf");
 
-const getBookFromShelf = () => {
+const getBookFromShelf = (request, h) => {
+	const {name, reading, finished} = request.query;
+
+	// // name query
+	// if (name) {
+	// 	const filteredBook = bookshelf.filter(book =>
+	// 		book.name.toLowerCase().includes(name.toLowerCase())
+	// 	);
+
+	// 	const books = filteredBook.map(book => {
+	// 		return {
+	// 			id: book.id,
+	// 			name: book.name,
+	// 			publisher: book.publisher,
+	// 		};
+	// 	});
+
+	// 	const response = {
+	// 		status: "success",
+	// 		data: {
+	// 			books: books,
+	// 		},
+	// 	};
+	// 	return response;
+	// }
+
+	// // reading query
+	// if (reading == 1) {
+	// 	const filteredBook = bookshelf.filter(book => book.reading === true);
+
+	// 	const books = filteredBook.map(book => {
+	// 		return {
+	// 			id: book.id,
+	// 			name: book.name,
+	// 			publisher: book.publisher,
+	// 		};
+	// 	});
+
+	// 	const response = {
+	// 		status: "success",
+	// 		data: {
+	// 			books: books,
+	// 		},
+	// 	};
+	// 	return response;
+	// } else if (reading == 0) {
+	// 	const filteredBook = bookshelf.filter(book => book.reading === false);
+
+	// 	const books = filteredBook.map(book => {
+	// 		return {
+	// 			id: book.id,
+	// 			name: book.name,
+	// 			publisher: book.publisher,
+	// 		};
+	// 	});
+
+	// 	const response = {
+	// 		status: "success",
+	// 		data: {
+	// 			books: books,
+	// 		},
+	// 	};
+	// 	return response;
+	// }
+
+	// if (finished) {
+	// 	const filteredBook = bookshelf.filter(book => book.finished === true);
+
+	// 	const books = filteredBook.map(book => {
+	// 		return {
+	// 			id: book.id,
+	// 			name: book.name,
+	// 			publisher: book.publisher,
+	// 		};
+	// 	});
+
+	// 	const response = {
+	// 		status: "success",
+	// 		data: {
+	// 			books: books,
+	// 		},
+	// 	};
+	// 	return response;
+	// } else if (!finished) {
+	// 	const filteredBook = bookshelf.filter(book => book.finished === false);
+
+	// 	const books = filteredBook.map(book => {
+	// 		return {
+	// 			id: book.id,
+	// 			name: book.name,
+	// 			publisher: book.publisher,
+	// 		};
+	// 	});
+
+	// 	const response = {
+	// 		status: "success",
+	// 		data: {
+	// 			books: books,
+	// 		},
+	// 	};
+	// 	return response;
+	// }
+
+	// No query
 	const books = bookshelf.map(book => {
 		return {
 			id: book.id,
 			name: book.name,
 			publisher: book.publisher,
+			query: {
+				name,
+				reading,
+				finished,
+			},
 		};
 	});
 

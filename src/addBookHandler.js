2371e8524c2754d0697a7bfbf51c3e7acf74a541
Dhani AM <dhani.a.mm@gmail.com>
2022-11-07 23:50:09 +0800

    submission criteria 1 completed

--- src/addBookHandler.js
+++ src/addBookHandler.js
@@ -1,8 +1,8 @@
-const {response} = require("@hapi/hapi/lib/validation");
 const {bookshelf} = require("./bookshelf");
 const {nanoid} = require("nanoid");
 
 const addBookToShelf = (request, h) => {
+	// JSON automatically converted into JS Object with HAPI
 	const {name, year, author, summary, publisher, pageCount, readPage, reading} =
 		request.payload;
 
@@ -26,12 +26,49 @@ const addBookToShelf = (request, h) => {
 		updatedAt,
 	};
 
-	const response = h.response({
-		status: "hello",
-		message: "sucess",
-		data: newBook,
-	});
-	return response;
+	// Check error
+	// when there's no name
+	if (!name) {
+		const response = h.response({
+			status: "fail",
+			message: "Failed adding book. Please fill out the name of the book.",
+		});
+		response.code(400);
+		return response;
+	}
+
+	// when readPage bigger than pageCount
+	if (readPage > pageCount) {
+		const response = h.response({
+			status: "fail",
+			message: "Failed adding book. readPage can't be bigger than pageCount",
+		});
+		response.code(400);
+		return response;
+	}
+
+	bookshelf.push(newBook);
+
+	const isSuccess = bookshelf.filter(book => book.id === id).length > 0;
+
+	if (isSuccess) {
+		const response = h.response({
+			status: "success",
+			message: "Book added to shelf",
+			data: {
+				bookId: newBook.id,
+			},
+		});
+		response.code(201);
+		return response;
+	} else {
+		const response = h.response({
+			status: "error",
+			message: "Failed adding book",
+		});
+		response.code(500);
+		return response;
+	}
 };
 
 module.exports = {addBookToShelf};

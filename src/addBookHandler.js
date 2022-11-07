1e45f77a7f700f4363eaa9272755ae493eee30b3
Dhani AM <dhani.a.mm@gmail.com>
2022-11-07 13:15:38 +0800

    added nanoid import which cause all the previous problem. problem fixed

--- src/addBookHandler.js
+++ src/addBookHandler.js
@@ -1,5 +1,6 @@
 const {response} = require("@hapi/hapi/lib/validation");
 const {bookshelf} = require("./bookshelf");
+const {nanoid} = require("nanoid");
 
 const addBookToShelf = (request, h) => {
 	const {name, year, author, summary, publisher, pageCount, readPage, reading} =
@@ -24,6 +25,13 @@ const addBookToShelf = (request, h) => {
 		insertedAt,
 		updatedAt,
 	};
+
+	const response = h.response({
+		status: "hello",
+		message: "sucess",
+		data: newBook,
+	});
+	return response;
 };
 
 module.exports = {addBookToShelf};
